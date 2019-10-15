20c2ed7d086b556861037f7a77527607b5695008
malthunayan <53401880+malthunayan@users.noreply.github.com>
2019-10-15 10:38:21 +0300

    Update admin.py

--- api/admin.py
+++ api/admin.py
@@ -5,13 +5,39 @@ from .models import Category, Product, Profile, Cart, CartItem
 class CartItemInline(admin.TabularInline):
 	model = CartItem
 
+	def get_extra(self, request, obj=None, **kwargs):
+		return 1
+
+
+class ProductInline(admin.TabularInline):
+	model = Product
+
+	def get_extra(self, request, obj=None, **kwargs):
+		return 1
+
+
 class CartAdmin(admin.ModelAdmin):
 	list_display=('profile','completed')
 	inlines = [
 		CartItemInline,
 	]
 
-admin.site.register(Category)
-admin.site.register(Product)
-admin.site.register(Profile)
+
+class CategoryAdmin(admin.ModelAdmin):
+	inlines = [
+		ProductInline,
+	]
+
+
+class ProductAdmin(admin.ModelAdmin):
+	list_display = ('name', 'price', 'quantity','manufacturer')
+
+
+class ProfileAdmin(admin.ModelAdmin):
+	list_display = ('user','bio','phone_num','gender')
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Profile, ProfileAdmin)
 admin.site.register(Cart, CartAdmin)
