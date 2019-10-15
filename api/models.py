485e9488d0753376e1f217a1b6a2c51a0f7cbb89
Mohammad Al Thunayan <malthunayan4@gmail.com>
2019-10-15 10:56:40 +0300

    Minor changes by MT

--- api/models.py
+++ api/models.py
@@ -5,63 +5,59 @@ from django.db.models.signals import post_save
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=50)
-    image = models.ImageField(blank=True)
+	name = models.CharField(max_length=50)
+	image = models.ImageField(blank=True)
 
-    def __str__(self):
-        return self.name
+	def __str__(self):
+		return self.name
 
 
 class Profile(models.Model):
-    user = models.OneToOneField(
-        User, related_name='profile', on_delete=models.CASCADE)
-    bio = models.TextField()
-    phone_num = models.CharField(max_length=8)
-    gender = models.CharField(max_length=10)
-    image = models.ImageField(blank=True, null=True)
+	user = models.OneToOneField(User, related_name='profile', on_delete=models.CASCADE)
+	bio = models.TextField()
+	phone_number = models.CharField(max_length=8)
+	gender = models.CharField(max_length=10)
+	image = models.ImageField(blank=True, null=True)
 
-    def __str__(self):
-        return self.user.username
+	def __str__(self):
+		return self.user.username
 
 
 class Cart(models.Model):
-    profile = models.ForeignKey(Profile, related_name='carts', on_delete=models.CASCADE, blank=True, null=True)
-    #change to completed
-    completed = models.BooleanField(default=False)
+	profile = models.ForeignKey(Profile, related_name='carts', on_delete=models.CASCADE, blank=True, null=True)
+	completed = models.BooleanField(default=False)
 
-    def __str__(self):
-        return self.profile.user.username + str(self.id)
+	def __str__(self):
+		return self.profile.user.username + str(self.id)
 
 
 class Product(models.Model):
-    price = models.PositiveIntegerField()
-    name = models.CharField(max_length=100)
-    quantity = models.PositiveIntegerField()
-    #create a model for manu and make it foreignKey
-    manufacturer = models.CharField(max_length=50)
-    description = models.TextField()
-    image = models.ImageField(blank=True, null=True)
-    category = models.ForeignKey(
-        Category, related_name="products", on_delete=models.CASCADE)
+	price = models.PositiveIntegerField()
+	name = models.CharField(max_length=100)
+	quantity = models.PositiveIntegerField()
+	# Create a model for manufacturer and make it ForeignKey
+	manufacturer = models.CharField(max_length=50)
+	description = models.TextField()
+	image = models.ImageField(blank=True, null=True)
+	category = models.ForeignKey(
+		Category, related_name="products", on_delete=models.CASCADE)
 
-    def __str__(self):
-        return self.name
+	def __str__(self):
+		return self.name
 
 class CartItem(models.Model):
-	#Change related name for both to cart_items
-    item = models.ForeignKey(
-        Product, related_name="cart_items", on_delete=models.CASCADE, blank=True, null=True)
-    cart = models.ForeignKey(
-        Cart, related_name="cart_items", on_delete=models.CASCADE)
-    quantity = models.PositiveIntegerField(default=1)
+	item = models.ForeignKey(
+		Product, related_name="cart_items", on_delete=models.CASCADE, blank=True, null=True)
+	cart = models.ForeignKey(
+		Cart, related_name="cart_items", on_delete=models.CASCADE)
+	quantity = models.PositiveIntegerField(default=1)
 
 
-    def __str__(self):
-        return self.item.name
+	def __str__(self):
+		return self.item.name
 
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
-    if created:
-        Profile.objects.create(user=instance)
-        # Cart.objects.create(profile=instance.profile)
+	if created:
+		Profile.objects.create(user=instance)
