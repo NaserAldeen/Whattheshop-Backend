d70e182b272cefa5bd1e84d18c31f9c64616d342
Ahmad Qmairah <AhmadQmairah@gmail.com>
2019-10-17 09:35:29 +0300

    asd

--- api/urls.py
+++ api/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView
 
 from .views import (
-	UserCreateAPIView,CartItemCreateView, CheckoutView,ProductListView,
-	ProductDetailView, MyTokenObtainPairView, PreviousOrdersListView, UserCart
+    UserCreateAPIView, CartItemCreateView, CheckoutView, ProductListView,
+    ProductDetailView, MyTokenObtainPairView, UserCart, CategoryListView,
+    ProfileView
 )
 
 
@@ -11,11 +12,12 @@ urlpatterns = [
     path('login/', MyTokenObtainPairView.as_view(), name='login'),
     path('register/', UserCreateAPIView.as_view(), name='register'),
     path('products/', ProductListView.as_view(), name='list-view'),
-    path('products/<int:product_id>/', ProductDetailView.as_view(), name='detail-view'),
-    path('orders_list/', PreviousOrdersListView.as_view(), name='orders-list'),
+    path('products/<int:product_id>/',
+         ProductDetailView.as_view(), name='detail-view'),
     path('add_product/', CartItemCreateView.as_view(), name='item-create'),
     path('get_cart/', UserCart.as_view(), name='cart'),
-
     path('checkout/', CheckoutView.as_view(), name='checkout'),
+    path('category/', CategoryListView.as_view(), name='category'),
+    path('profile/', ProfileView.as_view(), name='profile')
 
 ]
--- api/views.py
+++ api/views.py
@@ -5,48 +5,51 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 
 from .serializers import (
-	CartListSerializer, UserCreateSerializer, ProductListSerializer,CartItemCreateSerializer,
-	ProductDetailSerializer, MyTokenObtainPairSerializer
+    CartListSerializer, UserCreateSerializer, ProductListSerializer, CartItemCreateSerializer,
+    ProductDetailSerializer, MyTokenObtainPairSerializer, CategoryListSerializer, ProfileSerializer
 )
-from .models import Product, Cart, CartItem
+from .models import Product, Cart, CartItem, Category, Profile
 
 
 class MyTokenObtainPairView(TokenObtainPairView):
-	serializer_class = MyTokenObtainPairSerializer
+    serializer_class = MyTokenObtainPairSerializer
 
 
 class UserCreateAPIView(CreateAPIView):
-	serializer_class = UserCreateSerializer
+    serializer_class = UserCreateSerializer
 
 
 class ProductListView(ListAPIView):
-	queryset = Product.objects.all()
-	serializer_class = ProductListSerializer
+    queryset = Product.objects.all()
+    serializer_class = ProductListSerializer
+
+
+class CategoryListView(ListAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategoryListSerializer
 
 
 class ProductDetailView(RetrieveAPIView):
-	queryset = Product.objects.all()
-	serializer_class = ProductDetailSerializer
-	lookup_field = 'id'
-	lookup_url_kwarg = 'product_id'
+    queryset = Product.objects.all()
+    serializer_class = ProductDetailSerializer
+    lookup_field = 'id'
+    lookup_url_kwarg = 'product_id'
 
 
-class PreviousOrdersListView(ListAPIView):
-	serializer_class=CartListSerializer
-	permissions=[IsAuthenticated]
+class ProfileView(ListAPIView):
+    serializer_class = ProfileSerializer
+    permissions = [IsAuthenticated]
 
-	def get_queryset(self):
-		return Cart.objects.filter(profile=self.request.user.profile, completed=True)
+    def get_queryset(self):
+        return Profile.objects.filter(user=self.request.user)
 
 
 class UserCart(ListAPIView):
-	serializer_class=CartListSerializer
-	permissions=[IsAuthenticated]
+    serializer_class = CartListSerializer
+    permissions = [IsAuthenticated]
 
-	def get_queryset(self):
-		return Cart.objects.filter(profile=self.request.user.profile, completed=False)
-	
-		
+    def get_queryset(self):
+        return Cart.objects.filter(profile=self.request.user.profile, completed=False)
 
 
 # class UserCart(APIView):
@@ -60,40 +63,41 @@ class UserCart(ListAPIView):
 # 			return Response(CartListSerializer(cart).data)
 
 
-
 class CartItemCreateView(APIView):
 
-	def post(self, request, format=None):
-		if (request.user.is_anonymous):
-			return Response("Please login in order to add to your cart")
+    def post(self, request, format=None):
+        if (request.user.is_anonymous):
+            return Response("Please login in order to add to your cart")
 
-		validated_data = request.data
-		product_id = int(validated_data["item"])
-		quantity = int(validated_data['quantity'])
+        validated_data = request.data
+        product_id = int(validated_data["item"])
+        quantity = int(validated_data['quantity'])
 
-		cart, created = Cart.objects.get_or_create(profile=request.user.profile, completed=False)
-		# cart_item, created2 = CartItem.objects.get_or_create(item__id=product_id, cart=cart)
-		cart_item = CartItem.objects.filter(item__id=product_id, cart=cart)
-		if quantity == 0:
-			if not cart_item.exists():
-				return Response("You can't add 0 products to the cart!")
-			elif cart_item.exists():
-				cart_item.delete() 
-				return Response("Item deleted")
-		elif quantity > 0:	
-			if created:
-				cart.profile = request.user.profile
-				cart.save()
+        cart, created = Cart.objects.get_or_create(
+            profile=request.user.profile, completed=False)
+        # cart_item, created2 = CartItem.objects.get_or_create(item__id=product_id, cart=cart)
+        cart_item = CartItem.objects.filter(item__id=product_id, cart=cart)
+        if quantity == 0:
+            if not cart_item.exists():
+                return Response("You can't add 0 products to the cart!")
+            elif cart_item.exists():
+                cart_item.delete()
+                return Response("Item deleted")
+        elif quantity > 0:
+            if created:
+                cart.profile = request.user.profile
+                cart.save()
 
-			if not cart_item.exists():
-				cart_item = CartItem.objects.create(item=Product.objects.get(id=product_id), cart=cart, quantity=int(validated_data['quantity']))
-			else:
-				cart_item = cart_item[0]
-				cart_item.quantity =cart_item.quantity+ quantity
-				cart_item.save()
-			return Response(CartItemCreateSerializer(cart_item, context={'request':request}).data)
-		
-		
+            if not cart_item.exists():
+                cart_item = CartItem.objects.create(item=Product.objects.get(
+                    id=product_id), cart=cart, quantity=int(validated_data['quantity']))
+            else:
+                cart_item = cart_item[0]
+                cart_item.quantity = cart_item.quantity + quantity
+                cart_item.save()
+            return Response(CartItemCreateSerializer(cart_item, context={'request': request}).data)
+
+
 # class CartItemCreateView(APIView):
 # 	permission_classes = [IsAuthenticated,]
 
@@ -109,29 +113,28 @@ class CartItemCreateView(APIView):
 # 			cart_item.quantity = quantity
 # 		else:
 # 			if quantity == 0:
-# 				cart_item.delete() 
+# 				cart_item.delete()
 # 				return Response()
 # 			else:
 # 				cart_item.quantity += quantity
 # 		cart_item.save()
 # 		return Response(CartItemCreateSerializer(cart_item, context={'request':request}).data)
-		
-	
 
 
 class CheckoutView(APIView):
-	permission_classes = [IsAuthenticated,]
+    permission_classes = [IsAuthenticated, ]
 
-	def get(self, request, format=None):
-		try:
-			cart = Cart.objects.get(completed=False, profile=request.user.profile)
-		except:
-			return Response("What are you doing bro? You can't checkout when you don't have items in your cart!")
-		
-		if cart.cart_items.all().exists():
-			for cart_item in cart.cart_items.all():
-				prod = cart_item.item
-				prod.update_quantity(-cart_item.quantity)
-			cart.complete()
-			return Response("Checked out successfully!")
-		return Response("Add products to your cart")
+    def get(self, request, format=None):
+        try:
+            cart = Cart.objects.get(
+                completed=False, profile=request.user.profile)
+        except:
+            return Response("What are you doing bro? You can't checkout when you don't have items in your cart!")
+
+        if cart.cart_items.all().exists():
+            for cart_item in cart.cart_items.all():
+                prod = cart_item.item
+                prod.update_quantity(-cart_item.quantity)
+            cart.complete()
+            return Response("Checked out successfully!")
+        return Response("Add products to your cart")
